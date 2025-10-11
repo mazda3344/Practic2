98653cb20c46544f190b4c75051c46b8ef744c34
mazda3344 <razdor3344@gmail.com>
2025-10-11 19:44:27 +0300

    runner

---
+++ Assets/Scripts/Runner1.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Runner1 : MonoBehaviour
+{
+    public int pointIndex;
+    public List<Transform> pointList;
+    public Transform currentPoint;
+    public float speed = 10f;
+    void Start()
+    {
+        if (pointIndex >= 0 && pointIndex < pointList.Count)
+        {
+            currentPoint = pointList[pointIndex];
+        }
+        else
+        {
+            currentPoint = pointList[0];
+        }
+    }
+    void Update()
+    {
+        if (gameObject.transform.position == currentPoint.position)
+        {
+            pointIndex++;
+            if (pointIndex >= pointList.Count)
+            {
+                pointIndex = 0;
+            }
+            currentPoint = pointList[pointIndex];
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.position, Time.deltaTime * speed);
+    }
+}
