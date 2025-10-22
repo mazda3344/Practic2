79ce76247f6ba21bcf82c0d5c6f23c5e42ed5614
mazda3344 <razdor3344@gmail.com>
2025-10-22 19:51:13 +0300

    fisical

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -1,52 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Runner : MonoBehaviour
 {
-    public Transform point1;
-    public Transform point2;
-    public Transform point3;
-    public Transform point4;
-    public Transform point5;
-    public float Speed;
-    public bool Go;
-    private Vector3 target;
-    void Start()
-    {
-        target = point1.position;
-    }
-    void Update()
-    {
-        transform.LookAt(point1);
-        transform.Rotate(0, 0, 1);
-        if(Go)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * Speed);
-        }
-        if(transform.position == target)
-        {
-            if(target == point1.position)
-            {
-                target = point2.position;
-            }
-            else if(target == point2.position)
-            {
-                target = point3.position;
-            }
-            else if(target == point3.position)
-            {
-                target = point4.position;
-            }
-            else if(target == point4.position)
-            {
-                target = point5.position;
-            }
-            else if(target == point5.position)
-            {
-                target = point1.position;
-            }
-        }
-    }
+    public Transform wrist;
 }
--- Assets/Scripts/Runner2_1.cs
+++ Assets/Scripts/Runner2_1.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Runner2_1 : MonoBehaviour
 {
-    public List<Transform> runnersList;
+    public List<Runner> runnersList;
+    // public List<Transform> runnersList;
     public int runnersIndex;
-    public Transform currentRunner;
-    public Transform nextRunner;
+    public Runner currentRunner;
+    // public Transform currentRunner;
+    public Runner nextRunner;
+    // public Transform nextRunner;
     public float speed = 25f;
+    public GameObject fakel;
     void Start()
     {
-
         if (runnersIndex >= 0 && runnersIndex < runnersList.Count)
         {
             currentRunner = runnersList[runnersIndex];
@@ -22,11 +26,14 @@ public class Runner2_1 : MonoBehaviour
             currentRunner = runnersList[0];
             nextRunner = runnersList[1];
         }
+        fakel.transform.SetParent(currentRunner.wrist);
+        fakel.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
     }
+
     void Update()
     {
-        currentRunner.LookAt(nextRunner);
-        if (currentRunner.position == nextRunner.position)
+        currentRunner.transform.LookAt(nextRunner.transform);
+        if (currentRunner.transform.position == nextRunner.transform.position)
         {
             runnersIndex++;
             if (runnersIndex >= runnersList.Count)
@@ -35,7 +42,9 @@ public class Runner2_1 : MonoBehaviour
             }
             currentRunner = nextRunner;
             nextRunner = runnersList[runnersIndex];
+            fakel.transform.SetParent(currentRunner.wrist);
+            fakel.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
         }
-        currentRunner.position = Vector3.MoveTowards(currentRunner.position, nextRunner.position, Time.deltaTime * speed);
+        currentRunner.transform.position = Vector3.MoveTowards(currentRunner.transform.position, nextRunner.transform.position, Time.deltaTime * speed);
     }
 }
