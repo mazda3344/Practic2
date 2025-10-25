0814a1e8e639cd3a6ddb58c2e367795961a6f1f6
mazda3344 <razdor3344@gmail.com>
2025-10-25 19:41:18 +0300

    128 BadGuys

---
+++ Assets/Scripts/SuperMan.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperMan : MonoBehaviour
+{
+    public string tagBadGuy = "BadGuy";
+    // public int layerBadGuy = LayerMask.GetMask("BadGuy");
+
+    // void Reset()
+    // {
+    //     tagBadGuy = "BadGuy";
+    //     layerBadGuy = LayerMask.GetMask("BadGuy");
+    // }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log("SuperMan::OnCollisionEnter(); -1- collision.gameObject:" + collision.gameObject);
+        // Debug.Log("SuperMan::OnCollisionEnter(); -2- collision.gameObject.tag:" + collision.gameObject.tag);
+        // Debug.Log("SuperMan::OnCollisionEnter(); -3- tagBadGuy:" + tagBadGuy);
+        // Debug.Log("SuperMan::OnCollisionEnter(); -4- collision.gameObject.layer:" + collision.gameObject.layer);
+        // Debug.Log("SuperMan::OnCollisionEnter(); -5- layerBadGuy.value:" + layerBadGuy);
+        if (collision.gameObject.tag == tagBadGuy)
+        {
+            Debug.Log("BadGuy detected by Tag");
+        }
+        // if (collision.gameObject.layer == layerBadGuy)
+        // {
+        //     Debug.Log("BadGuy detected by Layer");
+        // }
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+}
