ffcf2d6155c29ce4e6395585ba543b719923ae1e
mazda3344 <razdor3344@gmail.com>
2025-11-22 18:37:08 +0300

    SuperMan

--- Assets/Scripts/billiard.cs
+++ Assets/Scripts/billiard.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TEST1 : MonoBehaviour
+public class billiard : MonoBehaviour
 {
     private void Start()
     {
